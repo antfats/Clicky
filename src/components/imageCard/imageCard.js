31d3a565256aabecc6a5de58396bec12600c722a
bob <antfats@hotmail.com>
2019-06-14 14:49:27 -0700

    everything is now working!

--- src/components/imageCard/imageCard.js
+++ src/components/imageCard/imageCard.js
@@ -4,7 +4,7 @@ import "./imgCard.css";
 const Imagecard = props => (
     <div className="card" onClick={props.imageClick}>
         <div className="img-container">
-            <img alt="#" className="card" src={require("../images/rawImg/" + props.image)} />
+            <img alt={props.image.replace(".jpg", "")} className="card" src={require("../images/rawImg/" + props.image)} />
         </div>
     </div>
 );
